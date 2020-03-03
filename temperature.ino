eb4133609ca88979e039d57ac9a9b961fca37451
Antonis Chatzimichail <49094336+AntonisChatzimichail@users.noreply.github.com>
2020-03-03 20:06:11 +0200

    Review code
    Review DHT11 code

--- temperature.ino
+++ temperature.ino
@@ -1,7 +1,7 @@
 #include <DHT.h>
 #include <DHT_U.h>
 
-#define DATA 4
+#define DATA 2
 #define LED1 5
 #define DHTTYPE DHT11
 
@@ -10,7 +10,7 @@ DHT dht = DHT(DATA, DHTTYPE);
 void setup() {
   pinMode(DATA, INPUT);
   Serial.begin(9600);
-  //pinMode(LED1, OUTPUT);
+  pinMode(LED1, OUTPUT);
   dht.begin();
 }
 
@@ -18,15 +18,15 @@ void loop() {
   float temp = dht.readTemperature();
   float hum = dht.readHumidity();
 
-  /*if (temp>20) {
+  if (temp>26.5) {
     digitalWrite(LED1, HIGH);
   }
   else {
     digitalWrite(LED1, LOW);
-  }*/
-  Serial.println("The temperature now is: ");
-  Serial.println(temp);
-  Serial.println("The humidity now is: ");
+  }
+  Serial.print("T: ");
+  Serial.print(temp);
+  Serial.print(", H: ");
   Serial.println(hum);
   delay(2000);
 }
